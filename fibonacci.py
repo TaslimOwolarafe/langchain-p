90cfca7d1e5235a3dc41cd6c81abbaddb221e184
Taslim Owolarafe <owolarafetaslim@gmail.com>
2023-08-24 20:01:11 +0100

    langch

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def generate_fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    
+    return fib_sequence
+
+# Example....
+num_terms = int(input("Enter the number of Fibonacci terms to generate: "))
+fibonacci_sequence = generate_fibonacci(num_terms)
+print("Fibonacci Sequence:", fibonacci_sequence)
